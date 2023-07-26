9a4594ef3f9240af1b8c59d1605688c843e37f6f
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-07-26 10:31:59 +0200

    setup lsp but not every language

---
+++ lua/zk/plugins/lsp/lspsaga.lua
@@ -0,0 +1,15 @@
+local saga_status, saga = pcall(require, "saga")
+if not saga_status then
+    return 
+end
+
+saga.init_lsp_saga({
+    move_in_saga = { prev = "<C-k", next = "<C-j>" },
+    finder_action_kays = {
+        open = "<CR>",
+    },
+    definition_action_keys = {
+        edit = "<CR>",
+    },
+
+})
