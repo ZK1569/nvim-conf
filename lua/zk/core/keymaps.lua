f6fe90f281871693ba3ac6546e8a1ff1cbda1643
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-06-28 20:42:12 +0200

    debug closing split screen with sd

--- lua/zk/core/keymaps.lua
+++ lua/zk/core/keymaps.lua
@@ -2,13 +2,13 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", {desc = "Exit insert mode with jk" })
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("n", "x", '"_x') -- When deleting a caractere do not copy it
-keymap.set("n", "U", ":redo<CR>", {desc = "Redo" }) -- Redo
+keymap.set("n", "U", ":redo<CR>", { desc = "Redo" }) -- Redo
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-keymap.set("n", "<leader>sd", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<leader>sd", ":b#|bd#<CR>", { silent = true, desc = "Close current buffer but not the split" })
