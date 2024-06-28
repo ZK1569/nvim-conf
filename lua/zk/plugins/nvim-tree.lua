ffb9349147c8572e37b382b1ee49247c79113e8c
Cristian Ursu <cristian.ursu.2001@gmail.com>
2024-06-28 09:42:02 +0200

    debug nvim-tree default selecting left screen

--- lua/zk/plugins/nvim-tree.lua
+++ lua/zk/plugins/nvim-tree.lua
@@ -1,34 +1,23 @@
-return{
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function() 
-        local nvimtree = require("nvim-tree")
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-        nvimtree.setup({
-            view = { 
-                width = 35, 
-                relativenumber = true,
-            },
-            actions = {
-                open_file = {
-                    window_picker = {
-                        enable = false, 
-                    },
-                },
-            },
-            filters = {
-                custom = { ".DS_Store" },
-            },
-            git = {
-                ignore = false,
-            },
-        })
+		nvimtree.setup({
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
 
-        local keymap = vim.keymap
+		local keymap = vim.keymap
 
-        keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Open file tree on the current file" })
-    end
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Open file tree on the current file" })
+	end,
 }
